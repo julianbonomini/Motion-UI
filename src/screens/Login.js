463be41b3d92a43e43661d90c318f78126980345
Julian Bonomini <julian.bonomini@gmail.com>
2018-05-26 03:20:00 -0300

    Login to register animations

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import { View, Text, Image, StyleSheet, Dimensions, Animated, Easing } from 'react-native'
-import { Form, Item, Label, Input } from 'native-base'
+import { View, Text, Image, StyleSheet, Dimensions, Animated, Easing, TouchableOpacity } from 'react-native'
+import { Form, Item, Label, Input, Button } from 'native-base'
 import { wp, hp } from '../utils/screen'
-import CustomButton from '../components/CustomButton'
+
+const ANIMATION_TIME = 500
 
 export default class Login extends React.Component {
 
@@ -12,6 +13,13 @@ export default class Login extends React.Component {
       top: new Animated.Value(350),
       loginOpacity: new Animated.Value(1),
       registrationOpacity: new Animated.Value(0),
+      registerPosition: new Animated.Value(hp(90)),
+      buttonsBorderWidth: new Animated.Value(3),
+      registrationTitleFontSize: new Animated.Value(hp(2)),
+      loginPosition: new Animated.Value(hp(80)),
+      registerButtonPositon: new Animated.Value(hp(190)),
+      registerButtonWidth: new Animated.Value(wp(80)),
+      iconPosition: new Animated.Value(hp(120)),
       loading: false,
     }
   }
@@ -21,24 +29,81 @@ export default class Login extends React.Component {
   }
 
   moveBackground = () => {
-    Animated.parallel([
-      Animated.timing(
-        this.state.loginOpacity,
-        {
-          toValue: 0,
-          duration: 500,
-          easing: Easing.inOut(Easing.ease)
-        }
-      ),
-      Animated.timing(
-        this.state.top,
-        {
-          toValue: -250,
-          duration: 1000,
-          easing: Easing.inOut(Easing.ease)
-        }
-      ),
+    Animated.sequence([
+      Animated.parallel([
+        Animated.timing(
+          this.state.loginOpacity,
+          {
+            toValue: 0,
+            duration: ANIMATION_TIME,
+            easing: Easing.inOut(Easing.ease)
+          }
+        ),
+        Animated.timing(
+          this.state.top,
+          {
+            toValue: -250,
+            duration: ANIMATION_TIME,
+            easing: Easing.inOut(Easing.ease)
+          }
+        ),
+        Animated.timing(
+          this.state.registerPosition,
+          {
+            toValue: hp(5),
+            duration: ANIMATION_TIME,
+            easing: Easing.inOut(Easing.ease)
+          }
+        ),
+        Animated.timing(
+          this.state.registrationTitleFontSize,
+          {
+            toValue: 50,
+            duration: ANIMATION_TIME,
+            easing: Easing.inOut(Easing.ease)
+          }
+        ),
+        Animated.timing(
+          this.state.loginPosition,
+          {
+            toValue: hp(0),
+            duration: ANIMATION_TIME,
+            easing: Easing.inOut(Easing.ease)
+          }
+        ),
+        Animated.timing(
+          this.state.registerButtonPositon,
+          {
+            toValue: hp(90),
+            duration: ANIMATION_TIME,
+            easing: Easing.inOut(Easing.ease)
+          }
+        ),
+        Animated.timing(
+          this.state.iconPosition,
+          {
+            toValue: hp(6),
+            duration: ANIMATION_TIME,
+            easing: Easing.inOut(Easing.ease)
+          }
+        )
+      ]),
+      Animated.parallel([
+        Animated.timing(
+          this.state.registrationOpacity,
+          {
+            toValue: 1,
+            duration: ANIMATION_TIME,
+            easing: Easing.inOut(Easing.ease)
+          }
+        ),
+
+      ])
     ]).start()
+  }
+
+  register = () => {
+    console.log('asd')
   }
 
   render() {
@@ -46,9 +111,8 @@ export default class Login extends React.Component {
       <View style={styles.container}>
         <Animated.Image style={[styles.image, { top: this.state.top }]} source={require('../assets/Backgrounds/Mountains.png')} />
         <Animated.Text style={[styles.title, { opacity: this.state.loginOpacity }]}>NEVER STOP HIKING</Animated.Text>
-        <Animated.Text style={[styles.title, styles.registrationTitle, { opacity: this.state.registrationOpacity }]}>REGISTRATION</Animated.Text>
         <View style={styles.contentContainer}>
-          <Animated.View style={[styles.form, { opacity: this.state.loginOpacity }]}>
+          <View style={[styles.form]}>
             <Form>
               <Item floatingLabel>
                 <Label>Username</Label>
@@ -58,7 +122,7 @@ export default class Login extends React.Component {
                 <Label>Password</Label>
                 <Input secureTextEntry={true} />
               </Item>
-              <Animated.View style={{opacity: this.state.registrationOpacity}}>
+              <Animated.View style={{ opacity: this.state.registrationOpacity }}>
                 <Item floatingLabel>
                   <Label>Confirm Password</Label>
                   <Input secureTextEntry={true} />
@@ -69,19 +133,23 @@ export default class Login extends React.Component {
                 </Item>
               </Animated.View>
             </Form>
-          </Animated.View>
+          </View>
         </View>
-        <View style={styles.buttons}>
-          <CustomButton onPress={this.login}>
-            {this.state.loading ?
-              <Text style={styles.text}>a</Text> :
-              <Text style={styles.text}>LOG IN</Text>
-            }
-          </CustomButton>
-          <CustomButton onPress={this.moveBackground}>
+        <Animated.View style={[styles.loginButton, { opacity: this.state.loginOpacity, top: this.state.loginPosition, width: wp(80) }]}>
+          <Button style={[styles.button, { borderWidth: 3 }]} onPress={this.login}>
+            <Text style={styles.text}>LOG IN</Text>
+          </Button>
+        </Animated.View>
+        <Animated.View style={[styles.register, { top: this.state.registerPosition }]}>
+          <TouchableOpacity onPress={this.moveBackground}>
+            <Animated.Text style={[styles.text, { fontSize: this.state.registrationTitleFontSize }]}>REGISTER</Animated.Text>
+          </TouchableOpacity>
+        </Animated.View>
+        <Animated.View style={[styles.loginButton, { top: this.state.registerButtonPositon, width: this.state.registerButtonWidth }]}>
+          <Button style={[styles.button, { borderWidth: 3 }]} onPress={this.register}>
             <Text style={styles.text}>REGISTER</Text>
-          </CustomButton>
-        </View>
+          </Button>
+        </Animated.View>
       </View>
     )
   }
@@ -103,23 +171,31 @@ const styles = StyleSheet.create({
   },
   title: {
     position: 'absolute',
-    top: 50,
+    top: hp(5),
     fontSize: 50,
     fontWeight: 'bold',
     textAlign: 'center'
   },
   registrationTitle: {
-    color: '#000'
+    color: '#fff'
   },
   form: {
     width: wp(80),
   },
-  buttons: {
-    flex: 1,
-    width: wp(80),
-    justifyContent: 'flex-end',
+  loginButton: {
+    position: 'absolute',
     alignItems: 'center',
     paddingBottom: hp(5)
+  },
+  button: {
+    backgroundColor: 'transparent',
+    borderColor: '#fff',
+    height: hp(6),
+    marginBottom: hp(2),
+    borderRadius: hp(10),
+  },
+  register: {
+    position: 'absolute',
   },
   text: {
     flex: 1,
