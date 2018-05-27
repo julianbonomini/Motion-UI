fc610dbd4d9121ed0952e34988098e74c1797a89
Julian Bonomini <julian.bonomini@gmail.com>
2018-05-27 07:25:44 -0300

    add credit card

--- src/navigators/RootNavigator.js
+++ src/navigators/RootNavigator.js
@@ -1,10 +1,14 @@
 import { StackNavigator } from 'react-navigation'
 import Login from '../screens/Login'
+import CreditCard from '../screens/CreditCard'
 
 const RootNaviator = StackNavigator({
+  CreditCard: {
+    screen: CreditCard
+  },
   Login: {
     screen: Login,
-  }
+  },
 }, {
   headerMode: 'none'
 })
